0c5a4c7175670cc4c7b714ecc1b2e2066fbd7bf2
Ali Megahed <112581413+PerpetualYeti@users.noreply.github.com>
2023-04-15 00:18:22 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
-if __name__ == "__main__":
-  main()
+from datetime import datetime
 
 def main():
-  print("hello")
+  name = str('Ali')
+  now = datetime.now()
+  
+  print(f'Hello, my name is {name}')
+  print(f'It is currently {now.strftime("%H:%M:%S")}')
+
+
+if __name__ == "__main__":
+  main()
